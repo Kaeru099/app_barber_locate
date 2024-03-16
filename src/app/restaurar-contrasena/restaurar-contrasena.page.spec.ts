3dd2aaca0cff42ec205a9f7a1ba8454873cec717
KarinaRestrepo <karinarestrepo092@gmail.com>
2024-03-16 17:08:34 -0500

    Interfaz restaurar contraseña funcional

--- src/app/restaurar-contrasena/restaurar-contrasena.page.spec.ts
+++ src/app/restaurar-contrasena/restaurar-contrasena.page.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 import { RestaurarContrasenaPage } from './restaurar-contrasena.page';
 
 describe('RestaurarContrasenaPage', () => {
